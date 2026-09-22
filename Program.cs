01599e97c000f8f860785dd21f2f75761fc00451
Abdelgawad Hamdy <34074927+abdelgawadhamdy2015@users.noreply.github.com>
2026-09-22 13:34:32 +0300

    Fix startup schema compatibility SQL

--- Program.cs
+++ Program.cs
@@ -76,8 +76,7 @@ using (var scope = app.Services.CreateScope())
 
     // The project currently uses EnsureCreated instead of EF migrations. Keep existing
     // databases compatible with the new per-user notification and report-owner columns.
-    db.Database.ExecuteSqlRaw(@"\
-IF COL_LENGTH('dbo.Users', 'EmployeeId') IS NULL
+    db.Database.ExecuteSqlRaw(@"IF COL_LENGTH('dbo.Users', 'EmployeeId') IS NULL
     ALTER TABLE [dbo].[Users] ADD [EmployeeId] int NULL;
 IF COL_LENGTH('dbo.Notifications', 'UserId') IS NULL
     ALTER TABLE [dbo].[Notifications] ADD [UserId] int NULL;");
